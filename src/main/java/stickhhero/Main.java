8c0c6c6f35455f65e95101c0a2a5402f1921ce2c
atharvwarade <115917225+atharvwarade@users.noreply.github.com>
2023-11-29 21:33:14 +0530

    Update Main.java

--- src/main/java/stickhhero/Main.java
+++ src/main/java/stickhhero/Main.java
@@ -1,7 +1,5 @@
 package stickhhero;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
@@ -10,14 +8,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
     private Stage stage;
-    private static Player player;
+    private static Player player = new Player();
 
     @Override
     public void start(Stage s) throws Exception {
@@ -28,33 +25,38 @@ public class Main extends Application {
         stage.setTitle("StickHero");
         stage.setScene(scene);
         stage.show();
-//        new Timeline(
-//                new KeyFrame(Duration.millis(20), event -> {
-//                    somefunction() // in this function at a later point the timeline will resume
-//                    timeline.pause()
-//                }
-//                )
-//        )
     }
 
-    public void mainMenu(Stage s) {
-
+    public void mainMenu(Stage s) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/firstScreen.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root,500,400);
+        stage.setTitle("StickHero");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     public void startGameScreen(MouseEvent mouseEvent) throws IOException {
+        player.setCoordinates();
         Node temp = (Node)mouseEvent.getSource();
         stage = (Stage)temp.getScene().getWindow();
-        Game controller = new Game(stage,player);
-        controller.startGameScene(player);
+        Game controller = new Game(stage,player, this);
+        controller.startGameScene();
     }
 
-    public void endGameScreen() {
-
+    @FXML
+    public void endGameScreen(Player curr_player) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gameOverScreen.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root,500,400);
+        stage.setTitle("GAME OVER");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        player = new Player();
+        //player = new Player();
         launch(args);
     }
 
@@ -62,7 +64,7 @@ public class Main extends Application {
 
     }
 
-    public void mainMenu(MouseEvent mouseEvent) {
+    public void mainMenu(MouseEvent mouseEvent) throws IOException {
         mainMenu((Stage)((Node)mouseEvent.getSource()).getScene().getWindow());
     }
 }
