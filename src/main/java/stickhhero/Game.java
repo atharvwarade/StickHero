c16f129f744ffaf3ab683624272dd679348bf0e6
atharvwarade <115917225+atharvwarade@users.noreply.github.com>
2023-12-10 00:21:24 +0530

    Update Game.java

--- src/main/java/stickhhero/Game.java
+++ src/main/java/stickhhero/Game.java
@@ -2,12 +2,20 @@ package stickhhero;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 
 import java.io.IOException;
 import java.util.Random;
@@ -24,10 +32,57 @@ public class Game {
     private Timeline newBoxTimeline;
     private Main main;
     private Player player;
+    private Label label;
+    private int currentScore;
+    private ImageView cherry;
+    private boolean cherryPresent;
+    private Label cherryLabel;
 
 //    public Game() {
 //
 //    }
+
+
+    public Label getCherryLabel() {
+        return cherryLabel;
+    }
+
+    public void setCherryLabel(Label cherryLabel) {
+        this.cherryLabel = cherryLabel;
+    }
+
+    public ImageView getCherry() {
+        return cherry;
+    }
+
+    public void setCherry(ImageView cherry) {
+        this.cherry = cherry;
+    }
+
+    public boolean isCherryPresent() {
+        return cherryPresent;
+    }
+
+    public void setCherryPresent(boolean cherryPresent) {
+        this.cherryPresent = cherryPresent;
+    }
+
+    public Label getLabel() {
+        return label;
+    }
+
+    public void setLabel(Label label) {
+        this.label = label;
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
+    }
+
     public Game(Stage stage, Player player, Main main) {
         this.stage = stage;
         this.player = player;
@@ -37,6 +92,13 @@ public class Game {
         this.second = new Box();
         this.anchorPane = new AnchorPane();
         this.main = main;
+        this.label = new Label();
+        this.cherry = new ImageView(new Image("file:C:\\Users\\athar\\OneDrive\\Desktop\\MemoryGame\\src\\main\\resources\\img\\cherry.png"));
+        this.cherry.setFitWidth(50);
+        this.cherry.setFitHeight(50);
+        this.setCherryPresent(false);
+        this.cherryLabel = new Label();
+        label.setText(String.valueOf(0));
     }
 
     public Stage getStage() {
@@ -119,16 +181,41 @@ public class Game {
         this.player = player;
     }
 
-    public void startGameScene() {
+    public void startGameScene(int currentScore) {
+        player.setCoordinates();
+        label.setLayoutX(400.0);
+        label.setText(String.valueOf(currentScore));
+        cherryLabel.setLayoutX(200);
+        cherryLabel.setText("Cherry : "+player.getCherries());
         Scene scene = new Scene(anchorPane,500,400);
         Random random = new Random();
         createPlatform(first,100.0,0.0,200.0);
         //double secondLayoutX = 101 + (490 - 101)*(random.nextDouble());
-        double secondLayoutX = first.getPlatform().getLayoutX() + 1 + first.getPlatform().getWidth() + (495 - (first.getPlatform().getLayoutX() + 1 + first.getPlatform().getWidth())) * random.nextDouble();
-        double secondWidth = (secondLayoutX+1) + (495-secondLayoutX-1)*(random.nextDouble()) - (secondLayoutX);
+        double secondLayoutX = first.getPlatform().getLayoutX() + 1 + first.getPlatform().getWidth() + (450 - (first.getPlatform().getLayoutX() + 1 + first.getPlatform().getWidth())) * random.nextDouble();
+        //double secondLayoutX = 120;
+        double secondWidth = (secondLayoutX+1) + (450-secondLayoutX-1)*(random.nextDouble()) - (secondLayoutX);
+        if (secondWidth <= player.getPlayerSkin().getFitWidth()) {
+            secondWidth = player.getPlayerSkin().getFitWidth();
+        }
+
         createPlatform(second,secondWidth,secondLayoutX,200.0);
-        anchorPane.getChildren().addAll(first.getPlatform(),second.getPlatform(),this.player.getPlayerSkin());
-        anchorPane.getChildren().add(line);
+        anchorPane.getChildren().addAll(first.getPlatform(),second.getPlatform(),this.player.getPlayerSkin(),label);
+        anchorPane.getChildren().addAll(line,cherry,cherryLabel);
+        Result result = JUnitCore.runClasses(TestClass.class);
+        if (secondLayoutX-first.getPlatform().getLayoutX()-first.getPlatform().getWidth() > 100) {
+            System.out.println(secondLayoutX-first.getPlatform().getLayoutX()+first.getPlatform().getWidth());
+            System.out.println(secondLayoutX);
+            System.out.println("CHERRY YES");
+            this.cherry.setLayoutX((100+secondLayoutX)/2);
+            this.cherry.setLayoutY(200);
+            this.setCherryPresent(true);
+            this.cherry.setVisible(true);
+        }
+        else {
+            System.out.println("CHERRY NO");
+            this.setCherryPresent(false);
+            this.anchorPane.getChildren().remove(cherry);
+        }
         stage.setScene(scene);
         stage.show();
         this.startGameLoop(player,scene);
@@ -137,10 +224,18 @@ public class Game {
     private void startGameLoop(Player player, Scene scene) {
         timeline = new Timeline(new KeyFrame(Duration.millis(10), event -> {
             System.out.println("STARTING GAME");
+            if (this.isCherryPresent() && !this.anchorPane.getChildren().contains(this.cherry)) {
+                this.anchorPane.getChildren().add(this.cherry);
+            }
+            if (!this.isCherryPresent() && this.anchorPane.getChildren().contains(this.cherry)) {
+                this.anchorPane.getChildren().remove(this.cherry);
+            }
             stick.extendLine(stage,player,scene,first,second);
-            player.translateTimeline(stage,scene, stick, first, second, this);
-            second.translateSecondRectangle(player);
-            first.translateFirstRectangle(player);
+            player.translateTimeline(scene, stick, first, second, this);
+            //second.translateSecondRectangle(player);
+            Box.translate(second,"second",player);
+            //first.translateFirstRectangle(player);
+            Box.translate(first,"first",player);
             player.getBack(stick);
             createNewBoxTimeline(player);
         })
@@ -168,12 +263,25 @@ public class Game {
             player.setBackTranslated(false);
             Random random = new Random();
             double secondLayoutX = second.getPlatform().getLayoutX() + 25 + second.getPlatform().getWidth() + (495 - (second.getPlatform().getLayoutX() + 25 + second.getPlatform().getWidth())) * random.nextDouble();
+            //double secondLayoutX = 120;
             double secondWidth = (secondLayoutX+1) + (495-secondLayoutX-1)*(random.nextDouble()) - (secondLayoutX);
+            if (secondWidth <= player.getPlayerSkin().getFitWidth()) {
+                secondWidth = player.getPlayerSkin().getFitWidth();
+            }
             createPlatform(first,secondWidth,secondLayoutX,200.0);
             Box temp = new Box();
             Box.copy(temp,second);
             Box.copy(second,first);
             Box.copy(first,temp);
+            if (secondLayoutX-first.getPlatform().getLayoutX()-first.getPlatform().getWidth() > 100) {
+                this.cherry.setLayoutX((100+secondLayoutX)/2);
+                this.cherry.setLayoutY(200);
+                this.setCherryPresent(true);
+                this.cherry.setVisible(true);
+            }
+            else {
+                this.setCherryPresent(false);
+            }
             timeline.play();
         }
     }
@@ -195,13 +303,23 @@ public class Game {
         if (second.getSecondTimeline() != null) {
             second.getSecondTimeline().stop();
         }
-
-        main.endGameScreen(player);
+        int currentValue = Integer.parseInt(this.getLabel().getText());
+        player.resetPlayer();
+        main.endGameScreen(player,currentValue);
     }
 
     public void createPlatform(Box box,double width, double layoutX,double layoutY) {
         box.getPlatform().setWidth(width);
         box.getPlatform().setLayoutX(layoutX);
         box.getPlatform().setLayoutY(layoutY);
+    }
+
+    public void cherryCollected() {
+        this.cherry.setVisible(false);
+    }
+
+
+    public void reviveScreen() throws IOException {
+        main.reviveScreen(this,this.currentScore);
     }
 }
